b82bc0b1589650806f68af9bf0e5b9de2c826921
Asis Rai <raia10@uni.coventry.ac.uk>
2017-04-05 22:57:29 +0100

    Controller & Gangoffour

---
+++ Asis-s-Stuff/RegistrationController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace Group_AJR___SBC_Booking_System
+{
+    internal class CustomerTable : Entities.User
+    {
+       
+        public MembershipType Membership { get; set; }
+
+        public CustomerTable(int ID)
+        {
+            this.ID = ID;
+        }
+
+        public static CustomerTable Fromsql(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDBFilename=|DataDirectory|\SBCDatabaseV2.mdf;Integrated Security=True;Connect Timeout=30");
+        {
+            Customer customer = new Customertoken["CustomerId"].ToObject<int>());
+            customer.Forename = token["firstname"].ToString();
+            customer.Surname = token["surname"].ToString();
+            customer.Address = token["address"].ToString();
+            customer.Email = token["ema"].ToString();
+            customer.PhoneNumber = token["phonenumber"].ToString();
+            customer.Membership = MembershipFromString(token["premember"].ToString());
+            return customer;
+        }
+
+        public static CustomerTable[] Fromsql(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDBFilename=|DataDirectory|\SBCDatabaseV2.mdf;Integrated Security=True;Connect Timeout=30");
+        {
+            Customer[] sessions = new Customer[tokens.Length];
+            for (int i = 0; i < sessions.Length; i++)
+                sessions[i] = Fromsql(tokens[i]);
+            return sessions;
+        }
+
+        public static MembershipType MembershipFromString(string membership)
+        {
+            switch (membership)
+            {
+                case "Loyalty Member":
+                    return MembershipType.Loyalty;
+                case "Free Member":
+                    return MembershipType.Free;
+                case "Registered":
+                    return MembershipType.Registered;
+                default:
+                    throw new ArgumentException("Invalid membership type", "membership");
+            }
+        }
+        public CustomerTable makeCopy()
+        {
+            return (Customer)this.MemberwiseClone();
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Forename, Surname);
+        }
+    }
+}
---
+++ Asis-s-Stuff/RegistrationGangofFour.cs
@@ -0,0 +1,72 @@
+﻿namespace Group_AJR___SBC_Booking_System.gof
+{
+    internal class RegistrationController
+    {
+        public static Registration[] Fetchpremember(int MembershipId)
+        {
+            //creates request from the sql server and prints reponse
+            Dictionary<string, object> Registration = new Dictionary<string, object>();
+            booking.Add("MembershipId", MembershipId);
+            DatabaseReply reply = DatabaseManager.DatabaseRequest("MembershipDisplay.sql", Registration);
+
+            //takes array of members from the resposne
+            return Prememeber.FromSql(reply.ReplyRows);
+        }
+
+        public static CustomerwithLoyalty[] GetCustomer(Registration premember)
+        {
+            DatabaseReply reply = DatabaseManager.DatabaseRequest("MembershipDisplay.sql",
+                new Dictionary<string, object>() { { "MembershipId", Registration.premember } });
+            Customer[] customers = Customer.FromSql(reply.ReplyRows);
+
+            Customer[] customerClone = CloneFactory.getCustomerClone(customers);
+
+            Registration.Customers = customers;
+            CustomerwithLoyalty[] CustomerwithLoyalty = new CustomerwithLoyalty[customers.Length];
+            for (int i = 0; i < customers.Length; i++)
+                CustomerwithLoyalty[i] = new CustomerwithLoyalty(customers[i], reply.ReplyRows[i]["CustomerId"].ToObject<int>());
+
+            return CustomerwithLoyalty;
+        }
+
+        public static CustomerwithFree[] GetCustomer(Registration premember)
+        {
+            DatabaseReply reply = DatabaseManager.DatabaseRequest("MembershipDisplay.sql",
+                new Dictionary<string, object>() { { "MembershipId", Registration.premember } });
+            Customer[] customers = Customer.FromSql(reply.ReplyRows);
+
+            Customer[] customerClone = CloneFactory.getCustomerClone(customers);
+
+            Registration.Customers = customers;
+            CustomerwithFree[] CustomerwithFree = new CustomerwithFree[customers.Length];
+            for (int i = 0; i < customers.Length; i++)
+                CustomerwithFree[i] = new CustomerwithFree(customers[i], reply.ReplyRows[i]["CustomerId"].ToObject<int>());
+
+            return CustomerwithFree;
+        }
+    }
+
+    internal class CustomerwithLoyalty
+    {
+        public readonly Customer Customer;
+        public readonly int prememebrID;
+
+        public CustomerwithLoyalty(Customer customer, int CustomerId)
+        {
+            this.Customer = customer;
+            this.prememberID = prememberID;
+        }
+    }
+
+    internal class CustomerwithFree
+    {
+        public readonly Customer Customer;
+        public readonly int prememebrID;
+
+        public CustomerwithFree(Customer customer, int CustomerId)
+        {
+            this.Customer = customer;
+            this.prememberID = prememberID;
+        }
+    }
+}
