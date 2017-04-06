73e1e1cd9449628087f9067694fdd35aa6406bb4
Asis Rai <raia10@uni.coventry.ac.uk>
2017-04-06 01:49:13 +0100

    Registration-automatedunittesting code C#
    Registration-automatedunittesting code C#

---
+++ Registration-automatedunittesting/UnitTest1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroupAJR_RegistrationUnittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUIRegistrationTest()
+        {
+            //Perform automated test
+            this.UIMap.RecordedMethodUpdate();
+
+            string rtxt = UIMap.UIUpdateRegistrationWindow.UITxtfirstnameWindow.textBox1.Text;
+            string rbox = UIMap.UIUpdateRegistrationWindow.UICheckboxWindow.checkBox1.ToString();
+            string rbutt = UIMap.UIUpdateRegistrationWindow.UIButtonWindow.button1.ToString();
+
+            this.UIMap.RecordedMethodSubmit();
+
+            //Validation that the data is there
+            Verify verify = new Verify();
+            bool i = verify.getRegistration(rtxt, rbox, rbutt);
+            if (i == false)
+            {
+                Assert.Fail();
+            }
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
+
